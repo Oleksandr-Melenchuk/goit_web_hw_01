61a348a793521c92dd08ea8d7d678e5fe7a9b22a
Oleksandr-Melenchuk <forandback91@gmail.com>
2025-04-15 15:34:43 +0300

    Delete goit_algo_hw_04 directory

--- goit_algo_hw_04/Home_work_assistent/input_data.py
+++
@@ -1,9 +0,0 @@
-#Приймає вхідні данні та передає 1 аргумент як команду 2 та 3 як параметри
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
-
-# Перевірка на валідність номеру
-def correct_number(number):
-    return number.lstrip('+').isdigit()
